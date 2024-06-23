6bc672a42f0c42cb0db8014adbccd1ece4816f19
Sten Heimbrodt <39519505+gespel@users.noreply.github.com>
2024-06-23 18:39:31 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+from multiprocessing import freeze_support
+
+import rawpy
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
+from renderer import Renderer
+
+def main():
+    r = Renderer(8, "DSC_0896.NEF")
+    img_arr = r.render_multicore()
+    Image.fromarray(img_arr).save("DSC_0896_GENERATED.tiff")
+    print("DONE!")
+
+if __name__ == '__main__':
+    freeze_support()
+    main()
---
+++ renderer.py
@@ -0,0 +1,71 @@
+import multiprocessing
+import random
+
+import numpy as np
+import rawpy
+from tqdm import tqdm
+import multiprocessing as mp
+
+
+class Renderer:
+    def __init__(self, num_worker, filename):
+        self.num_worker = num_worker
+        self.filename = filename
+        with rawpy.imread(filename) as raw:
+            self.rgb = raw.postprocess()
+
+    def render(self) -> np.array:
+        for x in tqdm(range(self.rgb.shape[0]), unit=' rows'):
+            for y in range(self.rgb.shape[1]):
+                pixel = self.rgb[x][y]
+                # print(pixel)
+                #if pixel[0] > 100:
+                #    pixel[0] = random.randint(0, 255)
+                r, g, b = pixel
+                self.rgb[x][y] = (r, b, b)
+        return self.rgb
+
+    def render_multicore(self):
+        height, width, channels = self.rgb.shape
+
+        job_queues = []
+        result_queue = multiprocessing.Queue()
+        for q in range(0, self.num_worker):
+            queue = multiprocessing.Queue()
+            job_queues.append(queue)
+        worker = []
+        for i in range(0, self.num_worker):
+            print(f"{i} worker started")
+            p = multiprocessing.Process(target=render_worker, args=(job_queues[i], result_queue,))
+            worker.append(p)
+            p.start()
+
+        for i in range(len(self.rgb)):
+            job_queues[i % self.num_worker].put((self.rgb[i], i))
+
+        results = []
+        #while not result_queue.empty():
+        for i in tqdm(range(len(self.rgb)), unit=' rows'):
+            results.append(result_queue.get())
+
+
+        for result in results:
+            self.rgb[result[1]] = result[0]
+        return self.rgb
+
+
+
+def render_worker(job_queue, result_queue):
+    while True:
+        job = job_queue.get()
+        line = job[0]
+        index = job[1]
+        length = len(line)
+        for x in range(length):
+            pixel = line[x]
+            #print(pixel)
+            if pixel[0] > 100:
+                pixel[0] = 0
+                pixel[1] = 0
+
+        result_queue.put((line, index))
